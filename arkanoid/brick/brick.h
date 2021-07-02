f1c97b5598fc37cfbbe9ca41a92ce58a4023dd09
dimitryku <diman.kuzmin347@gmail.com>
2021-07-03 01:05:57 +0300

    Увеличена графика, исправлены некоторые недочеты слитых веток

--- arkanoid/brick/brick.h
+++ arkanoid/brick/brick.h
@@ -4,8 +4,12 @@
 #include <brick/brickproperty.h>
 #include <random>
 #include <QObject>
+#include <QGraphicsItem>
+#include <publicconstants.h>
+#include <QPainter>
+
 //abstract?
-class Brick : public QObject
+class Brick : public QObject, public QGraphicsItem
 {
 
     Q_OBJECT
@@ -32,6 +36,11 @@ private:
     BrickProperty* property;
 //    bool bonus_flag;
 
+
+    // QGraphicsItem interface
+public:
+    virtual QRectF boundingRect() const;
+    virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
 };
 
 #endif // BRICK_H
