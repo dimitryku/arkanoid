dfe2f069fa7f95c4f9937b3a7c018a146421efa7
lex019020 <lexazombike72@gmail.com>
2021-07-04 16:03:02 +0300

    bonusBody 1st iteration

--- arkanoid/bonus/bonusbody.cpp
+++ arkanoid/bonus/bonusbody.cpp
@@ -1,6 +1,39 @@
 #include "bonusbody.h"
 
-BonusBody::BonusBody()
-{
 
+BonusBody::BonusBody(QVector2D pos, Bonus insideBonus)
+{
+    this->position = pos;
+    this->bonusInside = insideBonus;
+    itemShape.addRect(boundingRect());
+}
+
+void BonusBody::Move()
+{
+    this->position += PublicConstants::BonusBodySpeed;
+}
+
+Bonus BonusBody::getBonus()
+{
+    return this->bonusInside;
+}
+
+QPainterPath BonusBody::shape()
+{
+    itemShape.moveTo(position.x(), position.y());
+    return itemShape;
+}
+
+
+QRectF BonusBody::boundingRect() const
+{
+    QVector2D size = PublicConstants::BonusBodySize;
+    return QRectF(position.x() - size.x()/2, position.y() - size.y()/2,
+                  size.x(), size.y());
+}
+
+void BonusBody::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    painter->setBrush(QBrush(Qt::green));
+    painter->drawRect(this->boundingRect());
 }
