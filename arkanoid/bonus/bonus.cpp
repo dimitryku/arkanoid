9b9139bd78162fa4698757f812350fd50038ba0e
Vlados <60868875+DizelforClaSS@users.noreply.github.com>
2021-07-04 17:10:07 +0300

    fix

--- arkanoid/bonus/bonus.cpp
+++ arkanoid/bonus/bonus.cpp
@@ -14,7 +14,7 @@ Bonus::Bonus(int positive_chance)
         case 1: type_bonus = plus_ball;             break;
         case 2: type_bonus = add_life;              break;
         case 3: type_bonus = stick_platform;        break;
-        case 4: type_bonus = magnet_ball;                break;
+        case 4: type_bonus = magnet_ball;           break;
         case 5: type_bonus = uber_ball;             break;
         case 6: type_bonus = slow_ball;             break;
         case 7: type_bonus = fast_ball;             break;
