bedad7e0be5f4730d508174134e66c2f3e6746ae
dimitryku <diman.kuzmin347@gmail.com>
2021-07-05 21:55:25 +0300

    hotfix

--- arkanoid/brick/brickproperty.cpp
+++ arkanoid/brick/brickproperty.cpp
@@ -8,11 +8,7 @@ BrickProperty::BrickProperty()
     {
         pixmap.push_back(pix.copy(80 * i, 0, 80, 30));
     }
-    pixmap.push_back(pix.copy(0, 0, 80, 30));       // normal
-    pixmap.push_back(pix.copy(80, 0, 80, 30));      // golden
-    pixmap.push_back(pix.copy(160, 0, 80, 30));     // steel
-    pixmap.push_back(pix.copy(240, 0, 80, 30));     // tnt
-    pixmap.push_back(pix.copy(320, 0, 80, 30));     // forced
+    // normal   // golden   // steel    // tnt  // forced
     for(int i = 0; i < pixmap.size(); i++)
     {
         pixmap[i] = pixmap[i].scaled(40, 15);
